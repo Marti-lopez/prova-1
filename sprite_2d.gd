1ed4af5bd1f353b314a0b3e5e688d0f8363c68eb
Marti-lopez <205749810+Marti-lopez@users.noreply.github.com>
2025-04-25 09:14:00 +0200

    g

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
-var velocitat:= 200
+var vx:= 400
+var vy:= 200
+var vymax:= 4096
+var count:= 1
 
 func _ready() -> void:
-	position = Vector2(0, 0)
+	pass
 	
 
 func _process(delta: float) -> void:
-	var direccio := Vector2.ZERO
-	direccio = Input.get_vector("esquerra", "dreta", "amunt", "avall")
-	velocity = direccio * velocitat
+	velocity = Vector2(0, 0)
+	vy += 3200 * delta
+	if vy > vymax:
+		vy = vymax
+	if Input.is_action_pressed("amunt") and count >= 1:
+		vy = -800
+		count = 0
+	var moviment := Vector2(vx, vy)
+	velocity += moviment
 	move_and_slide()
-
-	
+	if is_on_floor_only():
+		count = 1
 	
 	
